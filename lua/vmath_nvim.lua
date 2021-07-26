f35c67aa14ed8e299add4200e26a10dc36b49191
max397574 <max.heinzer397@gmx.ch>
2021-07-26 16:11:06 +0200

    added options

--- lua/vmath_nvim.lua
+++ lua/vmath_nvim.lua
@@ -9,8 +9,16 @@ local function bigger(a,b)
   return a < b
 end
 
+local show_sum = true
+local show_average = true
+local show_lowest = true
+local show_highest = true
+local show_range = true
+local show_median = true
+local debug = false
+
 function M.calculate()
-  -- local start = vim.fn['reltime']()
+  local start = vim.fn['reltime']()
   local numbers = {}
   local start_pos = vim.fn.getpos("'<")
   local end_pos = vim.fn.getpos("'>")
@@ -36,15 +44,31 @@ function M.calculate()
   else
     median = (numbers[count/2]+numbers[count/2+1])/2
   end
-  print("Sum: " .. sum)
-  print("Count: " .. count)
-  print("Average: " .. average)
-  print("Lowest: " .. numbers[1])
-  print("Highest: " .. numbers[#numbers])
-  print("Range: " .. (numbers[#numbers]-numbers[1]))
-  print("Median: " .. median)
-  -- print("used time")
-  -- vim.cmd(string.format([[%s %s]], 'echo', vim.fn['reltimestr'](vim.fn['reltime'](start))))
+  if show_sum then
+    print("Sum: " .. sum)
+  end
+  if show_count then
+    print("Count: " .. count)
+  end
+  if show_average then
+    print("Average: " .. average)
+  end
+  if show_lowest then
+    print("Lowest: " .. numbers[1])
+  end
+  if show_highest then
+    print("Highest: " .. numbers[#numbers])
+  end
+  if show_range then
+    print("Range: " .. (numbers[#numbers]-numbers[1]))
+  end
+  if show_median then
+    print("Median: " .. median)
+  end
+  if (debug) then
+    print("used time")
+    vim.cmd(string.format([[%s %s]], 'echo', vim.fn['reltimestr'](vim.fn['reltime'](start))))
+  end
 end
 
 function M.init()
